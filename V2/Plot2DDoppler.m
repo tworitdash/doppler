d03b7ac64c3395462f04ade763986a1b70dc5056
Tworit Dash <tworitdash@ms3srv1.ewi.tudelft.nl>
2021-03-30 14:16:09 +0200

    changes

--- V2/Plot2DDoppler.m
+++ V2/Plot2DDoppler.m
@@ -1,11 +1,11 @@
-function [] = Plot2DDoppler(vel_axis, mean_Phi, Signal, SI, OI) 
-    SNR_interest = SNR(SI);
+function [] = Plot2DDoppler(vel_axis, mean_Phi, Signal, SI, OI, SNR_db, Omega_rpm) 
+    SNR_interest = SNR_db(SI);
     Omega_interest = Omega_rpm(OI);
-    
-    figure; surface(vel_axis, mean_Phi * 180/pi, abs(squeeze(Signal(OI).doppler(SI, :, :)))); shading flat; colorbar; %colormap('jet'); 
+    s = squeeze(mean(squeeze(Signal(OI).doppler(:, SI, :, :)), 1));
+    figure; surface(vel_axis, mean_Phi * 180/pi, abs(s)); shading flat; colorbar; %colormap('jet'); 
     ylabel('Azimuth Angle \Phi [deg]', 'FontSize', 16);
     xlabel('Velocity axis [m/s]', 'FontSize', 16)
-    zlabel('Normalized Doppler spectrum', 'FontSize', 16);
-    title(['Normalized Doppler spectrum at ', num2str(Omega_interest), 'RPM', ', SNR = ', num2str(SNR_interest), ' dB'], 'FontSize', 16);
+    zlabel('Doppler spectrum', 'FontSize', 16);
+    title(['Doppler spectrum at ', num2str(Omega_interest), 'RPM', ', SNR = ', num2str(SNR_interest), ' dB'], 'FontSize', 16);
 
 end
---
+++ V2/PlotDopplerSNR.m
@@ -0,0 +1,18 @@
+function [] = PlotDopplerSNR(SNR_db, v_mean_e, v_spread_e, OI, PI, Omega_rpm, mean_Phi)
+    Phi_interest = mean_Phi(PI) .* 180/pi;
+    Omega_interest = Omega_rpm(OI);
+    figure; plot(SNR_db, squeeze(v_mean_e(:, OI, PI)));
+    grid on;
+    xlabel('SNR [dB]', 'FontSize', 16)
+    ylabel('Error in Mean Doppler velocity [m/s]', 'FontSize', 16);
+    title(['Error in Mean Doppler velocity at ', num2str(Omega_interest), 'RPM', ', \Phi = ', num2str(Phi_interest), ' deg'], 'FontSize', 16);
+    
+    
+    figure; plot(SNR_db, squeeze(v_spread_e(:, OI, PI)));
+    grid on;
+    xlabel('SNR [dB]', 'FontSize', 16)
+    ylabel('Error in Doppler spectrum width [m/s]', 'FontSize', 16);
+    
+    title(['Error in Doppler spectrum width at ', num2str(Omega_interest), 'RPM', ', \Phi = ', num2str(Phi_interest), ' deg'], 'FontSize', 16);
+
+end
